cbc8ba56237078c46aefe66890eabf26ddf065e7
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-25 13:58:51 -0500

    Fixed Issue With Camera Not In The Right Position

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _player;
+    [SerializeField] private Transform _startCam;
+
+    private void Start()
+    {
+        Camera.main.transform.position = _startCam.transform.position;
+        Camera.main.transform.rotation = _startCam.transform.rotation;
+    }
     void Update()
     {
         transform.LookAt(_player);
