c48c496b2a169a2f4ddbeef3f87e9c0962029405
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-27 14:02:45 -0500

    Added Ripple Effect
    When you click anywhere, a ripple effect will show up to indicate where you clicked.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,17 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     private Animator _anim;
+    private SpriteRenderer _rippleRenderer;
     private bool _thrownCoin;
     private NavMeshAgent _agent;
+    [SerializeField] private Animator _rippleAnim;
     [SerializeField] private AudioClip _coinSound;
     [SerializeField] private GameObject _coinPrefab;
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _anim = GetComponentInChildren<Animator>();
+        _rippleRenderer = _rippleAnim.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -25,14 +28,22 @@ public class Player : MonoBehaviour
             RaycastHit hitInfo;
             if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo))
             {
-                if(clickedLeftMouseButton == true)
+                _rippleAnim.SetTrigger("Ripple");
+                GameObject ripple = _rippleAnim.gameObject;
+                ripple.transform.position = hitInfo.point;
+                if (clickedLeftMouseButton == true)
+                {
                     _agent.SetDestination(hitInfo.point);
+                    _rippleRenderer.color = new Color(255, 255, 255, 255);
+                }
                 else
                 {
                     StartCoroutine(ThrowRoutine());
                     _thrownCoin = true;
-                    GameObject coin = Instantiate(_coinPrefab, hitInfo.point, Quaternion.identity);
+                    GameObject coin = Instantiate(_coinPrefab, _rippleAnim.gameObject.transform.position, Quaternion.identity);
                     coin.transform.position = new Vector3(coin.transform.position.x, -2, coin.transform.position.z);
+                    ripple.transform.position = new Vector3(ripple.transform.position.x, -1.97f, ripple.transform.position.z);
+                    _rippleRenderer.color = new Color(242, 255, 0, 255);
                     AudioSource.PlayClipAtPoint(_coinSound, Camera.main.transform.position, 0.5f);
                     DistractClosestGuard(hitInfo.point);
                 }
